0cf58c6cf28b64c82b2065b9923b9ca3fdd9cc7d
n0uben <benjamin.noureux@gmail.com>
2022-09-30 18:02:27 +0200

    premier tests js et ajustements interface

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("coucou");
+function traduire() {
+    alert("letzongue la traduction !!!");
+}
+
+function initTraduction() {
+    const tradForm = document.querySelector("form");
+    tradForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        traduire();
+    });
+}
+
+initTraduction();

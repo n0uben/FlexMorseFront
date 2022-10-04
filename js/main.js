d64ca8bdde6826243f15492ee5a4e67c4a1c75a0
n0uben <benjamin.noureux@gmail.com>
2022-10-04 17:14:35 +0200

    ajoute switch languages + fetch mock traduction

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
-function traduire() {
-    alert("letzongue la traduction !!!");
+function traduire(event) {
+    const textToTranslate = event.target[0].value;
+
+    const inputLanguageValue = document.querySelector('#inputLanguage').innerHTML.toLowerCase();
+    const displayLanguageValue = document.querySelector('#displayLanguage').innerHTML.toLowerCase();
+
+    const UrlToFetch = `http://localhost:8080/traduction/${inputLanguageValue}/${displayLanguageValue}`;
+    const dataToSend = {
+        texteTraduit: textToTranslate
+    } 
+
+    fetch(UrlToFetch, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+          mode: 'cors',
+
+        body: JSON.stringify(dataToSend)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
 }
 
 function initEvents() {
     /*Event traduction*/
     const tradForm = document.querySelector("form");
+
     tradForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        traduire();
+        traduire(event);
     });
 
     /*Event switch langues*/
-    const btnSwitchLanguage = document.querySelector("#switchLangues");
-    btnSwitchLanguage.addEventListener("click", (event) => {
+    const btnSwitchLanguages = document.querySelector("#switchLanguages");
+    btnSwitchLanguages.addEventListener("click", (event) => {
         event.preventDefault();
-        switchLanguage();
+        switchLanguages();
     });
 }
 
-function switchLanguage() {
-    alert("olalala faut changer les langages");
+function switchLanguages(event) {
+    const inputLanguage = document.querySelector('#inputLanguage');
+    const displayLanguage = document.querySelector('#displayLanguage');
+
+    const inputLanguageValue = inputLanguage.innerHTML;
+    const displayLanguageValue = displayLanguage.innerHTML;
+
+    inputLanguage.innerHTML = displayLanguageValue;
+    displayLanguage.innerHTML = inputLanguageValue;
 }
 
 initEvents();
